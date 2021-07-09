b1086be64c5c42e53e400850a1ae7030e27a7771
Amen0920 <luj0605@gmail.com>
2021-07-09 21:29:22 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -11,7 +11,15 @@
             type: 'sticky',
             objs: {
                 container:document.querySelector('#scroll-section-0'),
+                messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+                messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+                messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+                messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+
             },
+            values: {
+                messageA_opacity: [0, 1]
+            }
         },
         {   
             //1
@@ -47,7 +55,7 @@
         for(let i = 0; i < scenInfo.length; i++){
             scenInfo[i].scrollHeight = scenInfo[i].heightNum * innerHeight;
             scenInfo[i].objs.container.style.height = `${scenInfo[i].scrollHeight}px`;
-        }
+        };
 
         let totalScrollHeight = 0;
         yOffset = pageYOffset;
@@ -56,10 +64,35 @@
             if(totalScrollHeight >= yOffset) {
                 currentScene = i;
                 break;
-            }
-        }
+            };
+        };
         document.body.setAttribute('id',`show-scene-${currentScene}`);
-    }
+    };
+
+    function calcValues(values, currentYOffset){
+
+    };
+
+    function playAnimation() {  
+        const values = scenInfo[currentScene].values;
+        const objs = scenInfo[currentScene].objs;
+        const currentYOffset = yOffset - prevScrollHeight;
+
+
+        switch (currentScen) {
+            case 0:
+                let messageA_opacity_0 = values.messageA_opacity[0];
+                let messageA_opacity_1 = values.messageA_opacity[1];
+                calcValues(values.messageA_opacity, )
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    };
 
 
    
@@ -67,7 +100,7 @@
         prevScrollHeight = 0;
         for(let i = 0; i < currentScene; i++){
             prevScrollHeight += scenInfo[i].scrollHeight;
-        }
+        };
         if(yOffset > prevScrollHeight + scenInfo[currentScene].scrollHeight) {
             currentScene ++;
             document.body.setAttribute('id',`show-scene-${currentScene}`);
@@ -75,10 +108,9 @@
             if(currentScene === 0)return;
             currentScene --;
             document.body.setAttribute('id',`show-scene-${currentScene}`);
-        }
-
-        
-    }
+        };  
+        playAnimation();
+    };
 
 
     window.addEventListener('scroll', () => {
